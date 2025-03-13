31654c6898f1aecfd9499d0aac2a7df2affbcd2f
dali05 <dali.laaroussi@gmail.Com>
2025-03-13 08:30:58 +0300

    first commit

--- backend/src/main/java/org/example/backend/HelloController.java
+++ backend/src/main/java/org/example/backend/HelloController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World update";
+        return "Hello World";
     }
 }
